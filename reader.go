0f8a4e0027c47b5a38cf8a600ae6ca0afd3cb167
Caleb Gardner <calebq23@gmail.com>
2024-09-20 20:10:33 -0500

    Re-added NewReaderAtOffset

--- reader.go
+++ reader.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"time"
 
+	"github.com/CalebQ42/squashfs/internal/toreader"
 	squashfslow "github.com/CalebQ42/squashfs/low"
 )
 
@@ -25,6 +26,10 @@ func NewReader(r io.ReaderAt) (*Reader, error) {
 		r: out,
 	}
 	return out, nil
+}
+
+func NewReaderAtOffset(r io.ReaderAt, offset int64) (*Reader, error) {
+	return NewReader(toreader.NewOffsetReader(r, offset))
 }
 
 func (r *Reader) ModTime() time.Time {
