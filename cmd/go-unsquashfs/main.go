e0c1309ed4bde952271f59be1760c13ac100502d
Anders F Björklund <anders.f.bjorklund@gmail.com>
2025-03-15 17:32:27 +0100

    Add list option to unsquashfs

--- cmd/go-unsquashfs/main.go
+++ cmd/go-unsquashfs/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/CalebQ42/squashfs"
@@ -11,6 +13,7 @@ import (
 
 func main() {
 	verbose := flag.Bool("v", false, "Verbose")
+	list := flag.Bool("l", false, "List")
 	ignore := flag.Bool("ip", false, "Ignore Permissions and extract all files/folders with 0755")
 	flag.Parse()
 	if len(flag.Args()) < 2 {
@@ -24,6 +27,17 @@ func main() {
 	r, err := squashfs.NewReader(f)
 	if err != nil {
 		panic(err)
+	}
+	if *list {
+		root := flag.Arg(1)
+		fs.WalkDir(r, ".", func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(filepath.Join(root, path))
+			return nil
+		})
+		return
 	}
 	op := squashfs.DefaultOptions()
 	op.Verbose = *verbose
